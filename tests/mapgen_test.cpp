e88034d4a966b739d0e2e728d3bf0ea1486e2c52
mininao <maxence_aici@yahoo.fr>
2016-04-11 17:45:21 +0200

    Setting up tests for MapGen

---
+++ tests/mapgen_test.cpp
@@ -0,0 +1,11 @@
+#include "mapgen/mapgen.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_ALTERNATIVE_INIT_API
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test) {
+	BOOST_CHECK(true);
+	BOOST_CHECK(true);
+}
