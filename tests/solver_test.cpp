4ded5a97058ba301c52c56acaeede58162f5958a
Minijackson <minijackson@riseup.net>
2016-05-03 20:54:43 +0200

    Adding unit tests with other heuristics

--- tests/solver_test.cpp
+++ tests/solver_test.cpp
@@ -1,4 +1,6 @@
+#include "heuristics/null.hpp"
 #include "heuristics/shortest_path.hpp"
+#include "heuristics/mst.hpp"
 #include "mapgen/mapgen.hpp"
 #include "solver/tspsolver.hpp"
 
@@ -37,7 +39,28 @@ int main(int argc, char* argv[]) {
 	return ::boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
 }
 
-BOOST_AUTO_TEST_CASE(everything_mst) {
+BOOST_AUTO_TEST_CASE(dumb_null) {
+	using TSPSolver        = awesome::TSPSolver<awesome::NullHeuristic>;
+	using MapGraph         = TSPSolver::MapGraph;
+	using ConstNode        = MapGraph::ConstNode_t;
+	using WeightedProperty = graph::WeightedProperty;
+
+	MapGraph map;
+
+	map.addEdges({{"a", "b", WeightedProperty{1}},
+	              {"b", "c", WeightedProperty{1}},
+	              {"c", "d", WeightedProperty{1}},
+	              {"d", "e", WeightedProperty{1}},
+	              {"e", "a", WeightedProperty{1}}});
+
+	TSPSolver solver(map);
+
+	std::list<ConstNode> candidate{map["a"], map["b"], map["c"], map["d"], map["e"], map["a"]};
+
+	BOOST_CHECK(solver.goForIt() == candidate);
+}
+
+BOOST_AUTO_TEST_CASE(dumb_shortest) {
 	using TSPSolver        = awesome::TSPSolver<awesome::ShortestPathHeuristic>;
 	using MapGraph         = TSPSolver::MapGraph;
 	using ConstNode        = MapGraph::ConstNode_t;
@@ -58,7 +81,42 @@ BOOST_AUTO_TEST_CASE(everything_mst) {
 	BOOST_CHECK(solver.goForIt() == candidate);
 }
 
-BOOST_AUTO_TEST_CASE(solver_insolvable) {
+BOOST_AUTO_TEST_CASE(dumb_mst) {
+	using TSPSolver        = awesome::TSPSolver<awesome::MSTHeuristic>;
+	using MapGraph         = TSPSolver::MapGraph;
+	using ConstNode        = MapGraph::ConstNode_t;
+	using WeightedProperty = graph::WeightedProperty;
+
+	MapGraph map;
+
+	map.addEdges({{"a", "b", WeightedProperty{1}},
+	              {"b", "c", WeightedProperty{1}},
+	              {"c", "d", WeightedProperty{1}},
+	              {"d", "e", WeightedProperty{1}},
+	              {"e", "a", WeightedProperty{1}}});
+
+	TSPSolver solver(map);
+
+	std::list<ConstNode> candidate{map["a"], map["b"], map["c"], map["d"], map["e"], map["a"]};
+
+	BOOST_CHECK(solver.goForIt() == candidate);
+}
+
+BOOST_AUTO_TEST_CASE(solver_insolvable_null) {
+	using awesome::MapGen;
+
+	using TSPSolver = awesome::TSPSolver<awesome::NullHeuristic>;
+	using MapGraph  = awesome::MapGen::MapGraph;
+
+	MapGraph map = MapGen::fromFile("solver_2.csv");
+	map.removeEdge(map["d"], map["c"]);
+	map.removeEdge(map["c"], map["d"]);
+	TSPSolver solver(map);
+
+	BOOST_CHECK_THROW(solver.goForIt(), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(solver_insolvable_shortest) {
 	using awesome::MapGen;
 
 	using TSPSolver = awesome::TSPSolver<awesome::ShortestPathHeuristic>;
@@ -72,7 +130,37 @@ BOOST_AUTO_TEST_CASE(solver_insolvable) {
 	BOOST_CHECK_THROW(solver.goForIt(), std::logic_error);
 }
 
-BOOST_AUTO_TEST_CASE(solver_sample_problem_1) {
+BOOST_AUTO_TEST_CASE(solver_insolvable_mst) {
+	using awesome::MapGen;
+
+	using TSPSolver = awesome::TSPSolver<awesome::MSTHeuristic>;
+	using MapGraph  = awesome::MapGen::MapGraph;
+
+	MapGraph map = MapGen::fromFile("solver_2.csv");
+	map.removeEdge(map["d"], map["c"]);
+	map.removeEdge(map["c"], map["d"]);
+	TSPSolver solver(map);
+
+	BOOST_CHECK_THROW(solver.goForIt(), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(solver_sample_problem_1_null) {
+	using awesome::MapGen;
+
+	using TSPSolver = awesome::TSPSolver<awesome::NullHeuristic>;
+	using MapGraph  = awesome::MapGen::MapGraph;
+	using ConstNode = MapGraph::ConstNode_t;
+
+	MapGraph map = MapGen::fromFile("solver_1.csv");
+	TSPSolver solver(map);
+
+	std::list<ConstNode> result   = solver.goForIt(),
+	                     expected = {map["e"], map["d"], map["b"], map["c"], map["a"], map["e"]};
+
+	BOOST_CHECK(result == expected);
+}
+
+BOOST_AUTO_TEST_CASE(solver_sample_problem_1_shortest) {
 	using awesome::MapGen;
 
 	using TSPSolver = awesome::TSPSolver<awesome::ShortestPathHeuristic>;
@@ -88,7 +176,39 @@ BOOST_AUTO_TEST_CASE(solver_sample_problem_1) {
 	BOOST_CHECK(result == expected);
 }
 
-BOOST_AUTO_TEST_CASE(solver_sample_problem_2) {
+BOOST_AUTO_TEST_CASE(solver_sample_problem_1_mst) {
+	using awesome::MapGen;
+
+	using TSPSolver = awesome::TSPSolver<awesome::MSTHeuristic>;
+	using MapGraph  = awesome::MapGen::MapGraph;
+	using ConstNode = MapGraph::ConstNode_t;
+
+	MapGraph map = MapGen::fromFile("solver_1.csv");
+	TSPSolver solver(map);
+
+	std::list<ConstNode> result   = solver.goForIt(),
+	                     expected = {map["b"], map["c"], map["a"], map["e"], map["d"], map["b"]};
+
+	BOOST_CHECK(result == expected);
+}
+
+BOOST_AUTO_TEST_CASE(solver_sample_problem_2_null) {
+	using awesome::MapGen;
+
+	using TSPSolver = awesome::TSPSolver<awesome::NullHeuristic>;
+	using MapGraph  = awesome::MapGen::MapGraph;
+	using ConstNode = MapGraph::ConstNode_t;
+
+	MapGraph map = MapGen::fromFile("solver_2.csv");
+	TSPSolver solver(map);
+
+	std::list<ConstNode> result   = solver.goForIt(),
+	                     expected = {map["c"], map["d"], map["e"], map["a"], map["b"], map["c"]};
+
+	BOOST_CHECK(result == expected);
+}
+
+BOOST_AUTO_TEST_CASE(solver_sample_problem_2_shortest) {
 	using awesome::MapGen;
 
 	using TSPSolver = awesome::TSPSolver<awesome::ShortestPathHeuristic>;
@@ -100,6 +220,22 @@ BOOST_AUTO_TEST_CASE(solver_sample_problem_2) {
 
 	std::list<ConstNode> result   = solver.goForIt(),
 	                     expected = {map["b"], map["a"], map["e"], map["d"], map["c"], map["b"]};
+
+	BOOST_CHECK(result == expected);
+}
+
+BOOST_AUTO_TEST_CASE(solver_sample_problem_2_mst) {
+	using awesome::MapGen;
+
+	using TSPSolver = awesome::TSPSolver<awesome::MSTHeuristic>;
+	using MapGraph  = awesome::MapGen::MapGraph;
+	using ConstNode = MapGraph::ConstNode_t;
+
+	MapGraph map = MapGen::fromFile("solver_2.csv");
+	TSPSolver solver(map);
+
+	std::list<ConstNode> result   = solver.goForIt(),
+	                     expected = {map["d"], map["c"], map["b"], map["a"], map["e"], map["d"]};
 
 	BOOST_CHECK(result == expected);
 }
