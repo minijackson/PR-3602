517d189a4da97cd7ba580d867ed7e372639d9e5d
Maxence Aïci <mininao@users.noreply.github.com>
2016-05-03 19:10:50 +0200

    Commenting shortest_path.hpp

--- src/heuristics/shortest_path.hpp
+++ src/heuristics/shortest_path.hpp
@@ -4,9 +4,9 @@
 
 namespace awesome {
 
-	/*! \brief ShortestPathHeuristic description
-	 *
-	 * Detailed description
+	/* The  Shortest Path Heuristic (Minimum Spanning Tree) inherits from the abstract heuristic class.
+	 * In the map where the visited cities are left out, it uses the product of the shortest edge and
+	 * the number of cities left to estimate the remaining cost.
 	 */
 	class ShortestPathHeuristic : public Heuristic {
 	public:
